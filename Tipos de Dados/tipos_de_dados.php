c886f0c86e80733e320bbc70425b3add8668f52f
hugoserra <tcarvalhotaichou@gmail.com>
2021-12-18 16:06:32 -0300

    Tipos De Dados PHP: 1

---
+++ Tipos de Dados/tipos_de_dados.php
@@ -0,0 +1,23 @@
+<?php
+
+
+#Tipos De Dados PHP
+#-------------------------
+
+
+#As variaveis em php podem ter diferentes valores
+#A mesma variavel pode ter tipos diferentes de dados
+
+$variavel = 100;
+$variavel = "cem";
+
+#APESAR DE SEREM VARIAVEIS DE TIPO IMPLICITO, É IMPORTANTE LEMBRAR:
+
+
+$inteiro = 100;               #int
+$float = 10.5;                #float (ponto flutuante)
+$bool = true;                 #true or false
+$string = "Ola Mundo!";       #string (cadeia de chars)
+$array = [1,2,3];             #Array, coleção de valores
+$pessoa = new Pessoa();       #Objeto com propriedades e metodos
+$nulo = null;                 #Variavel com valor nulo
